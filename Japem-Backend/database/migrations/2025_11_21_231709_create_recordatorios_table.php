28e24e10065fe7f2346337ec210716098444dd37
totygraal <toty.mrh@gmail.com>
2025-11-24 15:18:11 -0600

    24-11-25

---
+++ Japem-Backend/database/migrations/2025_11_21_231709_create_recordatorios_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recordatorios', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->date('date');
+            // El booleano por defecto en false para que inicie como "no hecho"
+            $table->boolean('done')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recordatorios');
+    }
+};
